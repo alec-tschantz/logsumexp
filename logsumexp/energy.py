02124eb9d8604a29bd43c8d6bea7cbeaaa3c5ad5
Alexander Tschantz <tschantz.alec@gmail.com>
2025-01-21 10:49:26 +0000

    abstract energy class

--- logsumexp/energy.py
+++ logsumexp/energy.py
@@ -1,99 +1,76 @@
 import equinox as eqx
-from jax import Array, numpy as jnp, nn, random
+from jax import Array, numpy as jnp, random as jr, nn
 
 
 class Energy(eqx.Module):
-    def __call__(self, *args) -> Array:
+
+    def measure(self, *args) -> Array:
         raise NotImplementedError
+
+    def __call__(self, *args) -> float:
+        m = self.measure(*args)
+        return -jnp.sum(nn.logsumexp(m, axis=1))
 
 
 class Mixture(Energy):
-    mu: Array
-    prec: Array
+    mu: Array  # (K, D)
+    prec: Array  # (D,)
 
     def __init__(self, K: int, D: int, key: Array):
-        k1, _ = random.split(key, 2)
-        self.mu = random.normal(12, (K, D))
+        k1, _ = jr.split(key, 2)
+        self.mu = jr.normal(k1, (K, D))
         self.prec = jnp.ones((D,))
 
-    def measure(self, x: Array, mu: Array) -> Array:
-        # x: (N, D), mu: (K, D)
-        diffs = x[:, None, :] - mu[None, :, :]  # (N, K, D)
-        return self.logpi[None, :] - 0.5 * jnp.sum(
-            self.prec * diffs**2, axis=-1
-        )  # (N, K)
-
-    def __call__(self, x: Array) -> Array:
-        # x: (N, D)
-        m = self.measure(x, self.mu)  # (N, K)
-        return -jnp.sum(nn.logsumexp(m, axis=1))
-
-
-class SelfAttention(Energy):
-    Wk: Array
-    Wq: Array
-
-    def __init__(self, D: int, key: Array):
-        k1, k2 = random.split(key, 2)
-        self.Wk = random.normal(k1, (D, D))
-        self.Wq = random.normal(k2, (D, D))
-
-    def measure(self, xk: Array, xq: Array) -> Array:
-        # xk: (N, D), xq: (N, D)
-        return xk @ self.Wk @ (xq @ self.Wq).T
-
-    def __call__(self, x: Array) -> Array:
-        # x: (N, D)
-        m = self.measure(x, x)  # (N, N)
-        return -jnp.sum(nn.logsumexp(m, axis=0))
+    def measure(self, x: Array) -> Array:
+        diffs = x[:, None, :] - self.mu[None, :, :]  # (N, K, D)
+        return -0.5 * jnp.sum(self.prec * diffs**2, axis=-1)  # (N, K)
 
 
 class CrossAttention(Energy):
-    Wk: Array
-    Wq: Array
+    Wk: Array  # (D, D)
+    Wq: Array  # (D, D)
 
     def __init__(self, D: int, key: Array):
-        k1, k2 = random.split(key, 2)
-        self.Wk = random.normal(k1, (D, D))
-        self.Wq = random.normal(k2, (D, D))
+        k1, k2 = jr.split(key, 2)
+        self.Wk = jr.normal(k1, (D, D))
+        self.Wq = jr.normal(k2, (D, D))
 
-    def measure(self, xk: Array, xq: Array) -> Array:
-        # xk: (Nk, D), xq: (Nq, D)
-        return xk @ self.Wk @ (xq @ self.Wq).T
-
-    def __call__(self, xk: Array, xq: Array) -> Array:
-        # xk: (Nk, D), xq: (Nq, D)
-        m = self.measure(xk, xq)  # (Nk, Nq)
-        return -jnp.sum(nn.logsumexp(m, axis=0))
+    def measure(self, xq: Array, xk: Array) -> Array:
+        Q = xq @ self.Wq  # (Nq, D)
+        K = xk @ self.Wk  # (Nk, D)
+        return Q @ K.T  # (Nq, Nk)
 
 
 class Hopfield(Energy):
-    def measure(self, x: Array, mem: Array) -> Array:
-        # x: (N, D), mem: (M, D)
-        return x @ mem.T
-
-    def __call__(self, x: Array, mem: Array) -> Array:
-        # x: (N, D), mem: (M, D)
-        m = self.measure(x, mem)  # (N, M)
-        return -jnp.sum(nn.logsumexp(m, axis=1))
+    def measure(self, x: Array, m: Array) -> Array:
+        return x @ m.T
 
 
 class SlotAttention(Energy):
-    Wk: Array
-    Wq: Array
+    Wk: Array  # (D, D)
+    Wq: Array  # (D, D)
 
     def __init__(self, D: int, key: Array):
-        k1, k2 = random.split(key, 2)
-        self.Wk = random.normal(k1, (D, D))
-        self.Wq = random.normal(k2, (D, D))
+        k1, k2 = jr.split(key, 2)
+        self.Wk = jr.normal(k1, (D, D))
+        self.Wq = jr.normal(k2, (D, D))
 
     def measure(self, x: Array, slots: Array) -> Array:
-        # x: (N, D), slots: (S, D)
-        k = x @ self.Wk  # (N, D)
-        q = slots @ self.Wq  # (S, D)
-        return k @ q.T  # (N, S)
+        K = x @ self.Wk  # (N, D)
+        Q = slots @ self.Wq  # (S, D)
+        return K @ Q.T  # (N, S)
 
-    def __call__(self, x: Array, slots: Array) -> Array:
-        # x: (N, D), slots: (S, D)
-        m = self.measure(x, slots)  # (N, S)
-        return -jnp.sum(nn.logsumexp(m, axis=1))
+
+class SelfAttention(Energy):
+    Wk: Array  # (D, D)
+    Wq: Array  # (D, D)
+
+    def __init__(self, D: int, key: Array):
+        k1, k2 = jr.split(key, 2)
+        self.Wk = jr.normal(k1, (D, D))
+        self.Wq = jr.normal(k2, (D, D))
+
+    def measure(self, x: Array) -> Array:
+        Q = x @ self.Wq
+        K = x @ self.Wk
+        return Q @ K.T
